2a0c4ba7ab705a8743d83666198f1c35f74f2de7
mfioran1 <maurice97f@gmail.com>
2020-10-31 10:06:37 -0700

    added helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user
+    helper_method :logged_in?
+    
+    
+    def log_in(user)
+        session[:user_id] = user.id
+      end
+    
+      def current_user
+        User.find_by(id: session[:user_id])
+      end
+    
+      def logged_in?
+        !!current_user
+      end
+    
+      def authenticate
+        redirect_to login_path if !logged_in?
+      end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            session[:user_id] = @user.id
+            log_in(@user)
             redirect_to @user
         else
             render :new
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
     end
 
     def show
+        authenticate
         @user = User.find_by_id(params[:id])
         redirect_to '/' if !@user 
     end
