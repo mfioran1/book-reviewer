b1cbe4599da7e4e0198a7bcb43651fa28215b9db
mfioran1 <maurice97f@gmail.com>
2020-10-31 09:48:45 -0700

    created user create

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   
+  
   root 'sessions#home'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+
+  delete '/logout' => 'sessions#destroy'
 
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
