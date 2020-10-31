ed69b906b58f0b2c3364c385a7d73a0896545293
mfioran1 <maurice97f@gmail.com>
2020-10-31 08:55:40 -0700

    created db

---
+++ db/migrate/20201031155521_create_catalogs.rb
@@ -0,0 +1,11 @@
+class CreateCatalogs < ActiveRecord::Migration[6.0]
+  def change
+    create_table :catalogs do |t|
+      t.string :name
+      t.text :description
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
